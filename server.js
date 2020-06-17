23997787a5fa38cc02f2f0f96dbacfb8566d4d8f
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-06-17 19:33:21 +0200

    Set up production build to deploy to Heroku

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 const app = express()
 
 // Connect Database
 connectDB()
 
-// Middleware
+// Middleware (body parser)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-	res.json({
-		msg: 'Welcome to the Contact Keeper API...',
-	})
-})
+// Commented out when finishing the complete App
+// app.get('/', (req, res) => {
+// 	res.json({
+// 		msg: 'Welcome to the Contact Keeper API...',
+// 	})
+// })
 
 // Define Routes
 app.use('/api/users', require('./routes/users'))
 app.use('/api/contacts', require('./routes/contacts'))
 app.use('/api/auth', require('./routes/auth'))
+
+// Serve static assets in production (To serve React in production)
+// If the app is production => load the static folder => serve the html page when hitting / (homepage)
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder (static assests that React builds, npm run build)
+	app.use(express.static('client/build'))
+
+	// Important to use this below the other routes because ('*') means all routes other than that we have currently
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
 
 const PORT = process.env.PORT || 5000
 
